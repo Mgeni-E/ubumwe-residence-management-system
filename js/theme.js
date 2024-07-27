8272bb9b3ea01db82032440d0b05d5dadbad5d0e
agigibairene <i.agigiba@alustudent.com>
2024-07-27 04:01:12 +0200

    light & dark mode

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-  
-    // Load theme from local storage
-    if (localStorage.getItem("theme") === "dark") {
+function toggleTheme() {
+  const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
+  const themeIcon = document.getElementById("theme-icon");
+
+
+  if (localStorage.getItem("theme") === "dark") {
       body.classList.add("dark-mode");
       themeToggle.checked = true;
-    }
-  
-    themeToggle.addEventListener("change", () => {
+      themeIcon.classList.remove("fa-sun");
+      themeIcon.classList.add("fa-moon");
+  }
+
+  themeToggle.addEventListener("change", () => {
       if (themeToggle.checked) {
-        body.classList.add("dark-mode");
-        localStorage.setItem("theme", "dark");
+          body.classList.add("dark-mode");
+          localStorage.setItem("theme", "dark");
+          themeIcon.classList.remove("fa-sun");
+          themeIcon.classList.add("fa-moon");
       } else {
-        body.classList.remove("dark-mode");
-        localStorage.setItem("theme", "light");
+          body.classList.remove("dark-mode");
+          localStorage.setItem("theme", "light");
+          themeIcon.classList.remove("fa-moon");
+          themeIcon.classList.add("fa-sun");
       }
-    });
   });
-  
+}
+
+document.addEventListener("DOMContentLoaded", toggleTheme);
