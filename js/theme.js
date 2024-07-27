107d860de40a8cee0a4e98911ea6bacce4c9cba1
Elvin Mgeni <e.mgeni@alustudent.com>
2024-07-27 02:28:59 +0200

    light/dark Mode

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const themeToggle = document.getElementById("theme-toggle");
-    const body = document.body;
-  
-    // Load theme from local storage
-    if (localStorage.getItem("theme") === "dark") {
-      body.classList.add("dark-mode");
-      themeToggle.checked = true;
-    }
-  
-    themeToggle.addEventListener("change", () => {
-      if (themeToggle.checked) {
-        body.classList.add("dark-mode");
-        localStorage.setItem("theme", "dark");
-      } else {
-        body.classList.remove("dark-mode");
-        localStorage.setItem("theme", "light");
-      }
-    });
-  });
-  
+// icons
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+// theme vars
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// icon toggling 
+const iconToggle = () => {
+  moonIcon.classList.toggle("display-none");
+  sunIcon.classList.toggle("display-none");
+};
+
+// Initial theme check 
+
+const themeCheck = () => {
+  if (userTheme === "dark" || (!userTheme && systemTheme.matches)) {
+    document.documentElement.classList.add("dark");
+    moonIcon.classList.add("display-none");
+    return;
+  } 
+  sunIcon.classList.add("display-none");
+};
+
+// manual Theme switch
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    iconToggle();
+    return;
+  }
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+  iconToggle();
+};
+
+// call theme switch on clicking buttons
+sunIcon.addEventListener("click", () => {
+  themeSwitch();
+});
+
+moonIcon.addEventListener("click", () => {
+  themeSwitch();
+});
+
+themeCheck();
